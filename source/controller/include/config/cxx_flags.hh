ebef685aac930dcf8c5ae8910fbf184d4e9308ea
Dhruvan <dhruvan.k05@gmail.com>
2026-02-25 01:39:44 -0500

    fix: update MSVC sanitizer flags to avoid false runtime errors

--- source/controller/include/config/cxx_flags.hh
+++ source/controller/include/config/cxx_flags.hh
@@ -100,7 +100,7 @@ constexpr CF noErrorReportingFlag{.gcc="-fno-error-report",
                                   .mingw="-fno-error-report"};
 constexpr CF SanitizeFlag{.gcc="-fsanitize=address,undefined,pointer-subtract",
                                     .clang="-fsanitize=undefined,bounds,function",
-                                    .msvc="/RTCc /RTC1 /sdl /RTCu /RTCsu /RTCs",
+                                    .msvc="/RTC1 /sdl",
                                     .mingw="-fsanitize=address,undefined"};
 constexpr CF None{.gcc="", .clang="", .msvc="", .mingw=""};
 }  // namespace cxx::flags
