07605e55d350948440e4b5a9145d54c7f2ea6957
CHAOSION <ojasat5@gmail.com>
2026-06-20 03:43:09 -0400

    [NFC] Restructured AST and Diagnostics

--- Scripts/gen_diag.py
+++ Scripts/gen_diag.py
@@ -94,7 +94,7 @@ def main():
         "////////////////////////////////////////////////////////////////////////////////",
         "",
         "import Support::EvalHash::*;",
-        "import Diag::DiagMeta::*;",
+        "import Diagnostics::DiagMeta::*;",
         "",
         "enum DiagID derives u32 {",
         "  Invalid = 0,"
